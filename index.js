6a73e3fe0b08332c3cce447e10e114e58b2e1ee0
binsoo28 <ltbinh2842001@gmail.com>
2024-12-18 11:54:03 +0700

    adjust table view, data
    adjust table view, data

--- index.js
+++ index.js
@@ -1,72 +1,74 @@
-const express = require('express');
-require('dotenv').config();
-const http = require('http');
-const { Server } = require('socket.io');
-const { io: ClientIO } = require('socket.io-client'); // Dùng để kết nối tới Python server
-const connectDb = require('./src/config/db');
-const DeviceCheck = require('./src/models/deviceCheck')
+const express = require("express");
+require("dotenv").config();
+const http = require("http");
+const { Server } = require("socket.io");
+const { io: ClientIO } = require("socket.io-client"); // Dùng để kết nối tới Python server
+const connectDb = require("./src/config/db");
+const DeviceCheck = require("./src/models/deviceCheck");
 
 connectDb();
 
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', './src/views');
+app.set("view engine", "pug");
+app.set("views", "./src/views");
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   const lastCheck = await DeviceCheck.findOne().sort({ checkedAt: -1 }).exec();
   const devices = lastCheck ? lastCheck.devices : [];
-  res.render('devices', { devices });
+  res.render("devices", { devices });
 });
-
 
 const server = http.createServer(app);
 const io = new Server(server);
 
 // Kết nối tới Python server
-const pythonSocket = ClientIO('http://localhost:5000'); // Python server chạy tại đây
+const pythonSocket = ClientIO("http://localhost:5000");
 
-pythonSocket.on('connect', () => {
-  console.log('Connected to Python server');
+pythonSocket.on("connect", () => {
+  console.log("Connected to Python server");
 });
 
-pythonSocket.on('response', (data) => {
-  console.log('Received from Python:', data);
+pythonSocket.on("response", (data) => {
+  console.log("Received from Python:", data);
 });
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
+io.on("connection", (socket) => {
+  console.log("Client connected");
 
   // Nhận yêu cầu từ client và chuyển tới Python server
-  socket.on('checking', (data) => {
-    console.log('Checking devices...');
-    pythonSocket.emit('checking'); // Gửi dữ liệu tới Python server
+  socket.on("checking", (data) => {
+    console.log("Checking devices...");
+    pythonSocket.emit("checking"); // Gửi dữ liệu tới Python server
   });
 
   // Nhận phản hồi từ Python và gửi lại client
-  pythonSocket.on('result', async (result) => {
-    // {
-    //   devices: {
-    // name: string;
-    // quantity: number;
-    // },
-    // checkedAt: datetime;
-    // }
+  pythonSocket.on("result", async (result) => {
+    console.log("Received result from Python:", result);
 
-    console.log('Received result from Python:', result);
-
-    const deviveCheck = new DeviceCheck(result);
-    await deviveCheck.save();
-    socket.emit('ml_response', result);
+    const deviceCheck = new DeviceCheck(result);
+    await deviceCheck.save();
+    socket.emit("ml_response", result);
+    io.emit("ml_response", result);
   });
 
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+
+  socket.on("python_data", async (data) => {
+    console.log("Received data from Python client:", data);
+
+    const deviceCheck = new DeviceCheck(data);
+    await deviceCheck.save();
+
+    socket.emit("ml_response", data);
+    io.emit("ml_response", data);
   });
 });
 
 server.listen(3000, () => {
-  console.log('Node.js server is running on port 3000');
+  console.log("Node.js server is running on port 3000");
 });
--- src/models/deviceCheck.js
+++ src/models/deviceCheck.js
@@ -1,17 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const deviceSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true
+const deviceSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    missing: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
-  quantity: {
-    type: Number,
-    required: true,
-    min: 0
-  }
-}, { _id: false });
+  { _id: false }
+);
 
 const deviceCheckSchema = new mongoose.Schema({
   devices: {
@@ -20,10 +28,10 @@ const deviceCheckSchema = new mongoose.Schema({
   },
   checkedAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-const DeviceCheck = mongoose.model('DeviceCheck', deviceCheckSchema);
+const DeviceCheck = mongoose.model("DeviceCheck", deviceCheckSchema);
 
 module.exports = DeviceCheck;
